54a8948dad1bed2c1d4864c365105ce27720f409
Yash-RajSingh <yashraj45202@gmail.com>
2022-12-29 03:16:10 +0530

    Fixes in the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,34 +12,47 @@ import {
 } from "./headerElements";
 import ProfileIcon from "../../assets/profile.png";
 import DownArrow from "../../assets/down-arrow.png";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  let param = window.location.href;
+  const [showHeader, setShowHeader] = useState(true);
   const { auth, setAuth } = useContext(AuthContext);
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (param.includes("login")) {
+      setShowHeader(false);
+    } else {
+      setShowHeader(true);
+    }
+  }, [param]);
   return (
     <>
-      <HeaderWrapper>
-        <HeaderSubWrapper>
-          <HeaderTitle>Ensemble</HeaderTitle>
-          {auth ? (
-            <UserHeaderWrapper>
-              <WorkspaceButton>
-                Workspaces
-                <BtnIcon src={DownArrow} />
-              </WorkspaceButton>
-              <CreateBtn>
-                <LoginText>Create +</LoginText>
-              </CreateBtn>
-              <UserProfileIcon src={ProfileIcon} />
-            </UserHeaderWrapper>
-          ) : (
-            <LoginButton>
-              <LoginText>Login</LoginText>
-            </LoginButton>
-          )}
-        </HeaderSubWrapper>
-      </HeaderWrapper>
+      {showHeader && (
+        <HeaderWrapper>
+          <HeaderSubWrapper>
+            <HeaderTitle>Ensemble</HeaderTitle>
+            {auth ? (
+              <UserHeaderWrapper>
+                <WorkspaceButton>
+                  Workspaces
+                  <BtnIcon src={DownArrow} />
+                </WorkspaceButton>
+                <CreateBtn>
+                  <LoginText>Create +</LoginText>
+                </CreateBtn>
+                <UserProfileIcon src={ProfileIcon} />
+              </UserHeaderWrapper>
+            ) : (
+              <LoginButton>
+                <LoginText onClick={() => navigate("/login")}>Login</LoginText>
+              </LoginButton>
+            )}
+          </HeaderSubWrapper>
+        </HeaderWrapper>
+      )}
     </>
   );
 };
